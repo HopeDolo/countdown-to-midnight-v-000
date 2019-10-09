52a1f5cafe62f57b16c00cd5f8fff2eaec923be9
Darius Stewart <darius.stewart31@gmail.com>
2019-10-09 05:31:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 def countdown(num)
-  counter = num 
+  counter = num
   while counter > 0
     puts "#{num} SECOND (S)!"
-    counter -= 1
+    num -= 1
   end
   puts "HAPPY NEW YEAR!"
 end
